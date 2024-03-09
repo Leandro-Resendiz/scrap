5f96f3c0c60a821a86a042bf8d8c09896380cc15
Leandro-Resendiz <159493735+Leandro-Resendiz@users.noreply.github.com>
2024-03-09 17:53:39 -0600

    Scrap basic

---
+++ scrap.py
@@ -0,0 +1,21 @@
+#   url -> http://soria-goig.org/fuentesymanantialesdesoria/ftes0015.htm
+
+
+import requests
+from bs4 import BeautifulSoup
+
+for num in range(15, 30):
+    url = f'http://soria-goig.org/fuentesymanantialesdesoria/ftes00{num}.htm'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        word = 'habilitado'
+
+        if word in soup.get_text().lower():
+            print(f'Se encuentra la palabra {word} en la url {url}')
+
+    else:
+        print('Error no se puede acceder a la página solicitada')
